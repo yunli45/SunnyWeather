95d1dce0fc6b42430fc852b3055a70c623f79dba
yunli45 <yunli_45@163.com>
2021-05-31 19:36:42 +0800

    将背景图和状态栏融合到一起,借助Material库完成，实现过程也比较麻烦。这里我准备教你另外一种更简单的实现式

--- app/src/main/java/com/ws/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/ws/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,5 +1,6 @@
 package com.ws.sunnyweather.ui.weather
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -24,7 +25,13 @@ class WeatherActivity : AppCompatActivity() {
     val viewModel by lazy { ViewModelProvider(this).get(WeatherViewModel::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_weather)
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            window.statusBarColor = Color.TRANSPARENT
+
+
+                setContentView(R.layout.activity_weather)
         if (viewModel.locationLng.isEmpty()) {
             viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
         }
